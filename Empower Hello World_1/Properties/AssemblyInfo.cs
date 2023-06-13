a7e1cd366e0600214b342fc5febacf47cd5cc482
alan.pellow <alan.pellow@bt.com>
2023-06-13 14:24:35 +0100

    Add project files.

---
+++ Empower Hello World_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower Hello World_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower Hello World_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9B219796-735A-4153-BC64-9FA2BC3E9A77")]
+[assembly: AssemblyVersion("1.0.0.0")]
